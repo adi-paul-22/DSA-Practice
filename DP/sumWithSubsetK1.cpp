3893d7e958a6846f441f8606707f01c9ed790bef
adi-paul-22 <ap4671@srmist.edu.in>
2023-02-01 00:59:01 +0530

    fixed code

--- DP/sumWithSubsetK1.cpp
+++ DP/sumWithSubsetK1.cpp
@@ -1,47 +1,38 @@
-#include<bits/stdc++.h>
+//count subsets with sum k
+//memoization method 1
+#include <bits/stdc++.h>
+
 using namespace std;
-int subsetSumUtil(int ind, int target, vector<int>&arr,
-vector<vector<int>> &dp){
-    if(target == 0) return dp[ind][target] = true;
+
+int findWaysUtil(int ind, int target, vector<int>& arr, vector<vector<int>> &dp){
+    if(target==0)
+        return 1;
     
-    if(ind==0){return dp[ind][target] = arr[0] == target;}
+    if(ind == 0)
+        return arr[0] == target;
     
-    if(dp[ind][target] != -1){
+    if(dp[ind][target]!=-1)
         return dp[ind][target];
-    }
+        
+    int notTaken = findWaysUtil(ind-1,target,arr,dp);
     
-    bool notTaken = subsetSumUtil(ind-1,target, arr,dp);
-    
-    bool taken = false;
-    if(arr[ind] <= target) taken = subsetSumUtil(ind-1,target - arr[ind],
-    arr,dp);
-    
-    return dp[ind][target] = notTaken || taken;
-}
-int minSSdiff(vector<int>&arr, int n){
-    int sum = 0;
-    
-    for(int i:arr) sum+=i;
-    
-    vector<vector<int>> dp(n,vector<int>(sum+1,-1));
-    
-    for(int i =0;i<=sum;i++){
-        bool dummy = subsetSumUtil(n-1,i,arr,dp);
-    }
-    
-    int mini = 1e9;
-    for(int i =0;i<=sum;i++){
-        if(dp[n-1][i] == true){
-            int diff = abs(i -(sum-i));
-            mini =  min(mini,diff);
-        }
-    }
-    return mini;
+    int taken = 0;
+    if(arr[ind]<=target)
+        taken = findWaysUtil(ind-1,target-arr[ind],arr,dp);
+        
+    return dp[ind][target]= notTaken + taken;
 }
 
-int main(){
-    vector<int> arr = {1,2,3,3,4};
-    
-    int n = arr.size();
-    cout<<"Minimum difference is: "<<minSSdiff(arr,n);
+int findWays(vector<int> &num, int k){
+    int n = num.size();
+    vector<vector<int>> dp(n,vector<int>(k+1,-1));
+    return findWaysUtil(n-1,k,num,dp);
+}
+
+int main() {
+
+  vector<int> arr = {1,2,2,3};
+  int k=3;
+                                 
+  cout<<"No. of subsets are : " <<findWays(arr,k);
 }
