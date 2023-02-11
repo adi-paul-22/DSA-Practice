c6376d16ef0ab973ed714222875ad756b2f734e5
adi-paul-22 <ap4671@srmist.edu.in>
2023-02-11 14:40:26 +0530

    printing the lcs

---
+++ DP/dp26/1.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+void lcs(string s1,string s2){
+    int n = s1.size();
+    int m = s2.size();
+    
+    vector<vector<int>> dp(n+1 , vector<int> (m+1,0));
+    for (int ind1 = 1; ind1 <= n; ind1++) {
+    for (int ind2 = 1; ind2 <= m; ind2++) {
+      if (s1[ind1 - 1] == s2[ind2 - 1])
+        dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
+      else
+        dp[ind1][ind2] = 0 + max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
+    }
+  }
+  
+  int len = dp[n][m];
+  int i = n;
+  int j = m;
+  int index = len - 1;
+  string str = "";
+  for(int k =0;k<len;k++){
+      str+='$';
+  }
+  while(i>0 && j>0){
+      if(s1[i-1] == s2[j-1]){
+          str[index--] = s1[i-1];
+          i--;
+          j--;
+      } else if(dp[i-1][j] > dp[i][j-1]) i--;
+      else j--;
+  }
+  cout<<str;
+}
+
+int main(){
+    string s1 = "acdstrin";
+    string s2 = "cedrin";
+    
+    cout<<"LCS is ";
+    lcs(s1,s2);
+}
