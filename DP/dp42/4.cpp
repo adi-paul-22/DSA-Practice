fb09793566cf016f2af038f9e53ccb75598987d4
adi-paul-22 <ap4671@srmist.edu.in>
2023-02-19 01:43:22 +0530

    added code

---
+++ DP/dp42/4.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int longestIncreasingSubsequence(int arr[], int n){
+    vector<int> dp(n,1);
+    
+    for(int i =0;i<=n-1;i++){
+        for(int prev = 0;prev <=i-1;prev++){
+            if(arr[prev]<arr[i]){
+                dp[i] = max(dp[i],1+dp[prev]);
+            }
+        }
+    }
+    
+    int ans = -1;
+    
+    for(int i=0;i<n;i++){
+        ans = max(ans,dp[i]);
+    }
+    
+    return ans;
+}
+
+int main(){
+    int arr[]={10,9,2,5,3,7,101,18};
+     int n = sizeof(arr)/sizeof(arr[0]);
+     
+     cout<<"The length of the longest increasing subsequence is "<<
+     longestIncreasingSubsequence(arr,n);
+    
+    
+}
